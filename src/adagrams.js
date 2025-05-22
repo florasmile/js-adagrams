04b4aac93ca23799da2b0b5056a2ac2f0acb2135
dehuihu <dehuihu@gmail.com>
2025-05-22 13:26:56 -0700

    Implemented scoreWord in wave_03

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,13 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const LETTER_VALUE = {
+  A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4,
+  I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3,
+  Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8,
+  Y: 4, Z: 10
+};
+
 export const drawLetters = () => {
   // generate a pile of letters
   let letters = []
@@ -83,6 +90,17 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  //define LETTER_VALUE as a global variable (top of file)
+  // iterate through word to add up all values
+  let score = 0;
+  for (const char of word.toUpperCase()) {
+    score += LETTER_VALUE[char];
+  };
+  // add addiontional 8 points for length 7, 8, 9, 10
+  if (word.length >= 7) {
+    score += 8;
+  };
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
